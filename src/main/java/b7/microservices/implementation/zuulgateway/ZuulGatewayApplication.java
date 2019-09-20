2882157aa435105486fe637ca64124bdb775094e
Savinay Singh <savinay.singh86@gmail.com>
2019-09-19 23:02:24 -0500

    zuul gateway

---
+++ src/main/java/b7/microservices/implementation/zuulgateway/ZuulGatewayApplication.java
@@ -0,0 +1,39 @@
+package b7.microservices.implementation.zuulgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import b7.microservices.implementation.zuulgateway.filters.ErrorFilter;
+import b7.microservices.implementation.zuulgateway.filters.PostFilter;
+import b7.microservices.implementation.zuulgateway.filters.PreFilter;
+import b7.microservices.implementation.zuulgateway.filters.RouteFilter;
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+	
+	 @Bean
+	    public PreFilter preFilter() {
+	        return new PreFilter();
+	    }
+	    @Bean
+	    public PostFilter postFilter() {
+	        return new PostFilter();
+	    }
+	    @Bean
+	    public ErrorFilter errorFilter() {
+	        return new ErrorFilter();
+	    }
+	    @Bean
+	    public RouteFilter routeFilter() {
+	        return new RouteFilter();
+	    }
+
+}
